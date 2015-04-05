45444eb1e56cd9ec2bec578a6086731c28d9aa53
Rudi Wijaya <rudi_mail90@yahoo.com>
2015-04-05 09:03:09 -0430

    hore bisa bikin JSON

--- vpercnode.cpp
+++ vpercnode.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 #include "SimpleAmqpClient/SimpleAmqpClient.h"
 
 using namespace std;
@@ -7,6 +9,15 @@ using namespace AmqpClient;
 
 int main(int argc, char **argv) {
 	cout << "Hello" << "World" << "\n";
+
+	using boost::property_tree::ptree;
+	ptree pt;
+	pt.put("Halo", "juga");
+	stringstream ss;
+	boost::property_tree::json_parser::write_json(ss, pt, true);
+	string jsonStr = ss.str();
+	cout << "JSON: " << jsonStr << "\n";
+
 	cout << "Connecting..." << "\n";
 	Channel::ptr_t channel = Channel::CreateFromUri("amqp://localhost");
 	string queue = channel->DeclareQueue("");
