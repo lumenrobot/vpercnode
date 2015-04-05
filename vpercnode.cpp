c023361a6cb7993e12e16ac9023e8b54e37c9e97
Rudi Wijaya <rudi_mail90@yahoo.com>
2015-04-05 08:54:30 -0430

    hore bisa publish :)

--- vpercnode.cpp
+++ vpercnode.cpp
@@ -1,7 +1,17 @@
 #include <string>
 #include <iostream>
+#include "SimpleAmqpClient/SimpleAmqpClient.h"
+
 using namespace std;
+using namespace AmqpClient;
 
 int main(int argc, char **argv) {
 	cout << "Hello" << "World" << "\n";
+	cout << "Connecting..." << "\n";
+	Channel::ptr_t channel = Channel::CreateFromUri("amqp://localhost");
+	string queue = channel->DeclareQueue("");
+	cout << "Publishing to '" << queue << "'..." << "\n";
+	BasicMessage::ptr_t message = BasicMessage::Create("wah keren");
+	channel->BasicPublish("", queue, message);
+	cout << "OK" << "\n";
 }
